7ecefc6125e6f7b365d42a8e3c011d3e6bff6be2
Jeremiah <jeremherring@gmail.com>
2023-10-21 01:52:42 -0700

    first commit

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What's Your Age?",
+        answer: "",
+        options: [
+            "0-17",
+            "18-55",
+            "55+"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What Is Your Gender?",
+        answer: "",
+        options: [
+            "Male",
+            "Female"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What Is Your Height",
+        answer: "",
+        options: [
+            "145-149cm",
+            "150-154cm",
+            "155-159cm",
+            "160-164cm",
+            "165-169cm",
+            "170-174cm",
+            "175-179cm",
+            "180-184cm",
+            "185-189cm",
+            "190-194cm"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What Is Your Ethnicity",
+        answer: "",
+        options: [
+            "American Indian or Alaskan Native",
+            "Asian",
+            "Black",
+            "Hispanic or Latino",
+            "Native Hawaiian or Other Pacific Islander",
+            "White"
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+            
+        ]
+    },
+]
