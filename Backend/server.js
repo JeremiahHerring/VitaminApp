28b475c01ea38b50eaf54a3634caa2a15f44dc7a
chawewo@gmail.com <chawewo@gmail.com>
2023-11-04 15:06:02 -0700

    Backend update

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,40 @@ const app = express()
 const PORT = 5000;
 // Declare routes in order to get from url
 
+app.post('/Api/calculateVitamins', (req, res) => {
+    const vitamins = req.body;
+  
+    // Define a function to query the database for vitamin information
+    function getVitaminInfo(vitamin) {
+      return new Promise((resolve, reject) => {
+        connection.query(
+          'SELECT * FROM vitamins WHERE name = ?',
+          [vitamin],
+          (err, results) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(results);
+            }
+          }
+        );
+      });
+    }
+  
+    // Use Promise.all to run the database queries concurrently for each vitamin
+    Promise.all(vitamins.map(getVitaminInfo))
+      .then(results => {
+        // Handle the results from the database queries
+        res.json(results);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred while querying the database.');
+      });
+});
+  
+
+
 app.get('/', (req,res) => {
     res.send('Hello there API') // we are going to use postman to create endpoints and testing
 })
