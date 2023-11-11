94b2ca0d469474b6effefaf3d7157a4eeb6b8c2d
Jeremiah <jeremherring@gmail.com>
2023-11-10 19:28:30 -0800

    Merge branch 'dev' of https://github.com/JeremiahHerring/VitaminApp into dev

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,21 @@
-const startBtn = document.querySelector('.start-btn');
-const header = document.querySelector(".header");
+const startBtn = document.querySelectorAll('.start-btn');
+const header = document.querySelector(".header"); 
+const navbarLinks = document.querySelectorAll('.navbar-link');
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
 
 // Function to detect scroll direction
 window.addEventListener('scroll', function () {
@@ -13,6 +28,43 @@ window.addEventListener('scroll', function () {
     }
 ;
 });
+
+window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    // Update active states of navigation links based on scroll position
+    updateActiveStates(scrollPosition);
+});
+
+// Function to find set navbar links active depending on scroll position
+function updateActiveStates(scrollPosition) {
+    const homeLink = document.getElementById('home-link');
+    const howItWorksLink = document.getElementById('how-it-works-link');
+    const aboutUsLink = document.getElementById('about-us-link');
+    const contactLink = document.getElementById('contact-link');
+
+    if (scrollPosition >= 0 && scrollPosition <= 700) {
+        setActive(homeLink);
+    } else if (scrollPosition > 700 && scrollPosition <= 1800) {
+        setActive(howItWorksLink);
+    } else if (scrollPosition > 1800 && scrollPosition <= 2000) {
+        setActive(aboutUsLink);
+    } else if (scrollPosition > 2000) {
+        setActive(contactLink);
+    }
+}
+
+// Function to set active class on the correct navigation link
+function setActive(link) {
+    // Remove 'active' class from all links
+    navbarLinks.forEach(navLink => navLink.classList.remove('active'));
+
+    // Add 'active' class to the specified link
+    link.classList.add('active');
+}
+
+
+
 
 startBtn.onclick = () => {
     window.location.href = 'quiz.html';
@@ -47,7 +99,6 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
-
 
 
 
