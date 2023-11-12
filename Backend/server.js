b27b6dcdf7bfcd368b1943adeae4d0d67da3a7b1
chawewo@gmail.com <chawewo@gmail.com>
2023-11-11 19:55:58 -0800

    Added testing feature

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 
 const connection = mysql2.createConnection({
-    host: "localhost",
+    host: "localhos",
     database: "vitaminsdb",
     user: "tester",
     password: "test"
@@ -20,11 +20,27 @@ const connection = mysql2.createConnection({
 
 
 const PORT = 3000;
+let testing = false; // This will define whether we are in testing mode or not
 app.post('/Api/calculateVitamins', async (req, res) => {
-
+  if (testing === true)
+  {
+    vitamins = [
+      {
+          "vitamin": {
+              "ID": 1,
+              "Vitamin_Name": "A",
+              "Description": "Vitamin A, also known as retinol, is a fat-soluble nutrient that plays a vital role in various physiological processes. It is crucial for maintaining the health and proper functioning of the body, but it is more specifically associated with its effects on vision, skin health, and cellular growth and development. Vitamin A is found in a variety of dietary sources, including fruits, vegetables, and animal-based foods.",
+              "Benefits_Description": " Vitamin A plays a crucial role in maintaining healthy vision, supporting the immune system, promoting proper cell growth and contributes to healthy skin.",
+              "Deficiency_Symptoms": "Night blindness, Dry skin, Susceptibility to infections",
+              "Test": "True"
+          }
+      }
+  ]
+  res.json(vitamins)
+  }
+  else {
   let vitamins = req.body
-
-if (vitamins.length === 0) {
+  if (vitamins.length === 0) {
     console.log("Empty Array") 
     return res.status(400).send('Invalid request: "Vitamins should be an array with elements');
   }
@@ -37,8 +53,8 @@ if (vitamins.length === 0) {
         if (err) {
           reject(err);
         } else {
-          const vitaminInfo = result.length > 0 ? result[0] : null;
-          resolve({[vitamin]: vitaminInfo });
+          const vitamin = result.length > 0 ? result[0] : null;
+          resolve({vitamin});
         }
       });
     });
@@ -52,7 +68,8 @@ if (vitamins.length === 0) {
     console.error('Error:', error);
     res.status(500).send('An error occurred while querying the database.');
   }
-});
+}}/*<--- This is the testing === true else{} */);
+
 app.get('/', (req,res) => {
     res.send('Hello there API') // we are going to use postman to create endpoints and testing
 })
@@ -61,8 +78,13 @@ app.get('/', (req,res) => {
 app.listen(PORT, ()=> {
     console.log(`SERVER    :  http://localhost:${PORT}`);
     connection.connect((err)=> {
-        if (err) throw err;
+        if (err){
+          console.log("No database connected, TESTING MODE")
+          testing = true
+        }
+        else {
         console.log("DATABASE CONNECTED");
+        }
     })
 })
 
